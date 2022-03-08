08282f7cab2ef477b16a6ee65ffab7e1b128540e
José Luis Pulgarín <jpulgarin@intelligencetv.co>
2022-03-08 12:05:39 -0500

    Implement filter functionality( for better)

--- src/components/FilterField.tsx
+++ src/components/FilterField.tsx
@@ -3,7 +3,8 @@ import React from "react"
 import { useNavigate } from "react-router-dom";
 import { BsPlusSquare } from "react-icons/bs";
 
-export const FilterField = () => {
+export const FilterField = ( props: any) => {
+
     const nav = useNavigate();
 
     const handleCreate = () => {
@@ -13,7 +14,7 @@ export const FilterField = () => {
     return (
         <div className="grid gap-2">
             <div>
-                <input className="w-full p-3 border rounded-md active:shadow-lg focus:shadow-lg" type="text" placeholder="Find your task" />
+                <input onChange={(event) => props.parentCallback(event)} className="w-full p-3 border rounded-md active:shadow-lg focus:shadow-lg" type="text" placeholder="Find your task" />
             </div>
             <div className="flex justify-between gap-2">
                 <button className="w-full bg-black text-white p-2 hover:opacity-80 active:ring-4 cursor-pointer rounded-md flex gap-2 justify-center items-center" onClick={handleCreate}><BsPlusSquare />Create new task</button>
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSupabase } from '../hooks/useSupabase';
 import { Task } from '../interfaces/supabase.interface';
 import { FilterField } from './FilterField';
@@ -6,15 +6,35 @@ import { TaskCard } from './TaskCard';
 
 export const Todos = () => {
     const { tasksList } = useSupabase();
+    const [customTask, setCustomTask] = useState([]);
+
+    const handleSearch = (event: any) => {
+        const value = event.target.value.toLowerCase();
+        if (value) {
+            let result = [];
+
+            console.log('call');
+            result = customTask?.filter((newData: any) => {
+                return Object.values(newData).join('').toLowerCase().includes(value.toLowerCase())
+            });
+            setCustomTask(result);
+        } else {
+            setCustomTask(tasksList);
+        }
+    }
+
+    useEffect(() => {
+        setCustomTask(tasksList);
+    }, [tasksList]);
 
     return (
         <div className="grid justify-center w-full pt-12 px-6 gap-4">
             <p className="font-bold text-3xl">👋 Welcome to ioterra Todo </p>
-            <FilterField />
+            <FilterField parentCallback={handleSearch} />
             <div className="w-full grid gap-2 ">
                 <p>Lastest tasks</p>
-                {tasksList?.length > 0 ? <>
-                    {tasksList?.map((props: Task) => {
+                {customTask?.length > 0 ? <>
+                    {customTask?.map((props: Task) => {
                         return <TaskCard key={props.id} {...props} />
                     })}
                 </> : <p>You dot have a taks</p>}
