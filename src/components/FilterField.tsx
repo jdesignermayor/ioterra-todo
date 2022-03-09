cb922062fcdabf7718c3901e160536087abf9443
José Luis Pulgarín <jpulgarin@intelligencetv.co>
2022-03-09 13:28:58 -0500

    add button to clean the filter

--- src/components/FilterField.tsx
+++ src/components/FilterField.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 
 import { useNavigate } from "react-router-dom";
-import { BsPlusSquare } from "react-icons/bs";
+import { BsPlusSquare, BsFilterCircle } from "react-icons/bs";
 
-export const FilterField = ( props: any) => {
-
+export const FilterField = (props: any) => {
     const nav = useNavigate();
 
     const handleCreate = () => {
@@ -13,10 +12,12 @@ export const FilterField = ( props: any) => {
 
     return (
         <div className="grid gap-2">
+            <p>Find by title</p>
             <div>
                 <input onChange={(event) => props.parentCallback(event)} className="w-full p-3 border rounded-md active:shadow-lg focus:shadow-lg" type="text" placeholder="Find your task" />
             </div>
             <div className="flex justify-between gap-2">
+                <button className="w-full bg-white text-black border p-2 hover:bg-slate-200 active:ring-4 cursor-pointer rounded-md flex gap-2 justify-center items-center" onClick={(event) => props.parentCallbackClear(event)}><BsFilterCircle />Clear filter</button>
                 <button className="w-full bg-black text-white p-2 hover:opacity-80 active:ring-4 cursor-pointer rounded-md flex gap-2 justify-center items-center" onClick={handleCreate}><BsPlusSquare />Create new task</button>
             </div>
         </div>
