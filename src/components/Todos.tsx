fb8f50ba72d2f62262b189038a9c3764732d6ff7
José Luis Pulgarín <jpulgarin@intelligencetv.co>
2022-03-09 13:28:44 -0500

    update filter function

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,22 +5,23 @@ import { FilterField } from './FilterField';
 import { TaskCard } from './TaskCard';
 
 export const Todos = () => {
-    const { tasksList } = useSupabase();
+    const { tasksList, getTasksByQuery } = useSupabase();
     const [customTask, setCustomTask] = useState([]);
 
-    const handleSearch = (event: any) => {
+    const handleSearch = async (event: any) => {
         const value = event.target.value.toLowerCase();
         if (value) {
-            let result = [];
-
-            console.log('call');
-            result = customTask?.filter((newData: any) => {
+            const result = customTask?.filter((newData: any) => {
                 return Object.values(newData).join('').toLowerCase().includes(value.toLowerCase())
             });
             setCustomTask(result);
         } else {
             setCustomTask(tasksList);
         }
+    }
+
+    const clearSearch = () => {
+        setCustomTask(tasksList);
     }
 
     useEffect(() => {
@@ -30,8 +31,8 @@ export const Todos = () => {
     return (
         <div className="grid justify-center w-full pt-12 px-6 gap-4">
             <p className="font-bold text-3xl">👋 Welcome to ioterra Todo </p>
-            <FilterField parentCallback={handleSearch} />
-            <div className="w-full grid gap-2 ">
+            <FilterField parentCallback={handleSearch} parentCallbackClear={clearSearch} />
+            <div className="grid gap-2 ">
                 <p>Lastest tasks</p>
                 {customTask?.length > 0 ? <>
                     {customTask?.map((props: Task) => {
